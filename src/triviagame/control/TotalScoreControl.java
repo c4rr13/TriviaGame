bb2b14571140d80d13acff8a16962d6aebd4c61f
carri_000 <carri_000@CarrieAnn>
2015-02-11 23:39:55 -0700

    Control

---
+++ src/triviagame/control/TotalScoreControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triviagame.control;
+
+/**
+ *
+ * @author carri_000
+ */
+public class TotalScoreControl {
+    public double TotalScore(SavedScore,Points)
+            if (SavedScore < 0) {
+            return -1;
+                    }
+if (Points < 0 ){
+return -1;
+}
+
+TotalScore = SavedScore + Points;
+    
+return TotalScore;
+
+
+    
+}
