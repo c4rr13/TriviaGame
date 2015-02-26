82e57c1fd543e4d1fc8265a03caab4c310562fe5
whitbillman <whitbillman@10.50.100.239>
2015-02-25 19:13:52 -0700

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ test/triviagame/control/TotalTimeTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triviagame.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author whitbillman
+ */
+public class TotalTimeTest {
+    
+    public TotalTimeTest() {
+    }
+
+    /**
+     * Test of TotalTime method, of class TotalTime.
+     */
+    @Test
+    public void testTotalTime() {
+        System.out.println("TotalTime");
+        double CountryTime = 0.0;
+        double PopTime = 0.0;
+        double RockTime = 0.0;
+        TotalTime instance = new TotalTime();
+        double expResult = 0.0;
+        double result = instance.TotalTime(CountryTime, PopTime, RockTime);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
